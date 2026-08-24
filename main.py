b1f0c985a189517d3fd553899c860b8a32bf4ce9
nguyen-james <jnguyen3852@gmail.com>
2026-08-23 17:47:15 -0700

    setup project

---
+++ main.py
@@ -0,0 +1,16 @@
+import keyboard, pyperclip, json
+
+
+def intialize_hotkeys():
+    
+    
+
+def main():
+    intialize_hotkeys()
+    while True:
+        #listen for hotkeys on execute them
+        
+
+
+if __name__ = "__main__":
+    main()
