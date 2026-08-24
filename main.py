315d8413b91cf70332b1615c156359dca77d35e1
nguyen-james <jnguyen3852@gmail.com>
2026-08-23 17:59:36 -0700

    load data from json file

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import keyboard, pyperclip, json
 
 
+
+def load_data():
+    
+    data_path = "data.json"
+
+    with open (data_path, "r") as file:
+        data = json.load(file)
+        return data
+
+   
+
 def intialize_hotkeys():
+    pass
+
+
+
     
     
 
 def main():
-    intialize_hotkeys()
-    while True:
-        #listen for hotkeys on execute them
+
+    data = load_data()
+
+    print(data)
+
+    
+    
         
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
